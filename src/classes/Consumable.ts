3426734f58ca88b03f7de20aedb42ea7ed26362b
Amber <678866+amberwilson@users.noreply.github.com>
2020-02-01 21:21:36 -0400

    Fix issue with Consumable type not being settable

--- src/classes/Consumable.ts
+++ src/classes/Consumable.ts
@@ -1,23 +1,24 @@
-import cuid from "cuid";
+import cuid from 'cuid';
 
-import Rating from "./Rating";
+import Rating from './Rating';
 
 enum ConsumableType {
-  BoardGame = "BOARD_GAME",
-  Book = "BOOK",
-  Movie = "MOVIE",
-  Show = "SHOW",
-  VideoGame = "VIDEO_GAME"
+  BoardGame = 'BOARD_GAME',
+  Book = 'BOOK',
+  Movie = 'MOVIE',
+  Show = 'SHOW',
+  VideoGame = 'VIDEO_GAME'
 }
 
 abstract class Consumable {
   uid: string;
   title?: string;
-  type?: ConsumableType;
   rating?: Rating;
   releaseDate?: Date;
   createdAt: Date = new Date();
   editedAt?: Date;
+
+  abstract get type(): ConsumableType;
 
   constructor() {
     this.uid = cuid();
