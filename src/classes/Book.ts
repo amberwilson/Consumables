b7a3829f5d26efcfaa265f4432a6908fc96bd239
Amber <678866+amberwilson@users.noreply.github.com>
2020-02-01 23:05:03 -0400

    Add type getter to Book

--- src/classes/Book.ts
+++ src/classes/Book.ts
@@ -1,22 +1,26 @@
-import Consumable from "./Consumable";
+import Consumable, { ConsumableType } from './Consumable';
 
 enum BookGenre {
-  ActionAndAdventure = "ACTION_AND_ADVENTURE",
-  Classics = "CLASSICS",
-  ComicBook = "COMIC_BOOK",
-  GraphicNovel = "GRAPHIC_NOVEL",
-  DetectiveAndMystery = "DETECTIVE_AND_MYSTERY",
-  Fantasy = "FANTASY",
-  HistoricalFiction = "HISTORICAL_FICTION",
-  Horror = "HORROR",
-  LiterayFiction = "LITERARY_FICTION",
-  Romance = "ROMANCE",
-  ScienceFiction = "SCIENCE_FICTION",
-  SuspenseAndThrillers = "SUSPENSE_AND_THRILLERS"
+  ActionAndAdventure = 'ACTION_AND_ADVENTURE',
+  Classics = 'CLASSICS',
+  ComicBook = 'COMIC_BOOK',
+  GraphicNovel = 'GRAPHIC_NOVEL',
+  DetectiveAndMystery = 'DETECTIVE_AND_MYSTERY',
+  Fantasy = 'FANTASY',
+  HistoricalFiction = 'HISTORICAL_FICTION',
+  Horror = 'HORROR',
+  LiterayFiction = 'LITERARY_FICTION',
+  Romance = 'ROMANCE',
+  ScienceFiction = 'SCIENCE_FICTION',
+  SuspenseAndThrillers = 'SUSPENSE_AND_THRILLERS'
 }
 
 class Book extends Consumable {
   author?: string;
+
+  get type(): ConsumableType {
+    return ConsumableType.Book;
+  }
 }
 
 export default Book;
